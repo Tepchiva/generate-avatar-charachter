9d728d8c9a5e2b5d688bffed3bcaabc4a81ed661
chiva_tep <chiva_tep@phillipbank.com.kh>
2023-07-06 16:43:22 +0700

    fix generate image with duplicate charater

--- generate-avatar.js
+++ generate-avatar.js
@@ -4,11 +4,11 @@ import fs from "fs";
 registerFont("fonts/Euclid Circular B SemiBold.ttf", {family: "Euclid SemiBold"})
 
 // dimensions for the image
-const width = 48;
-const height = 48;
+const width = 144;
+const height = 144;
 
 // font style
-const fontStyle = '15px "Euclid SemiBold"'
+const fontStyle = '45px "Euclid SemiBold"'
 const fontColor = "#FFFFFF";
 const bgColors = ['#6D5176','#8794BF','#E3A193','#D8BF86','#6ECCAF','#737B63','#334945','#375F48','#557153','#009EFF','#CB8F91','#7D5AA4','#4B79F3','#F8AB8E','#B7B4AA','#D5A956','#A5A582','#C6B870','#F88020','#E79B67','#F7931E','#FFA45C','#ECB65F','#00000029','#454EAB','#B27FA0','#674EB4','#6809EA','#2146C7','#DB528A','#38145E']
 
@@ -25,10 +25,10 @@ fs.mkdir('avatars', { recursive: true }, (err) => {
     }
 });
 
-const canvas = createCanvas(width, height);
-const context = canvas.getContext("2d");
 
 avatarNameList.forEach(avatarName => {
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext("2d");
     // instantiate the canvas object    
     context.fillStyle = bgColors[Math.floor(Math.random() * bgColors.length)];
     context.fillRect(0, 0, canvas.width, canvas.height); // draw a background color
@@ -43,7 +43,10 @@ avatarNameList.forEach(avatarName => {
     const buffer = canvas.toBuffer("image/png");
 
     const fileName = `avatars/${avatarName}.png`;
-    fs.writeFileSync(fileName, buffer)
+    fs.writeFile(fileName, buffer, (err) => {
+        if(err) 
+            console.log(`${fileName} was failed.`)
+    })
 });
 
 console.log(`Generating avatar profile have been end with '${avatarNameList.length}' avatars`);
