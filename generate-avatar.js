ac4a6124c72c917166a5fe7953c84a88915ea0cc
chiva_tep <chiva_tep@phillipbank.com.kh>
2023-07-06 14:29:03 +0700

    generate avatar feature

---
+++ generate-avatar.js
@@ -0,0 +1,49 @@
+import {createCanvas, registerFont} from "canvas";
+import fs from "fs";
+
+registerFont("fonts/Euclid Circular B SemiBold.ttf", {family: "Euclid SemiBold"})
+
+// dimensions for the image
+const width = 48;
+const height = 48;
+
+// font style
+const fontStyle = '15px "Euclid SemiBold"'
+const fontColor = "#FFFFFF";
+const bgColors = ['#6D5176','#8794BF','#E3A193','#D8BF86','#6ECCAF','#737B63','#334945','#375F48','#557153','#009EFF','#CB8F91','#7D5AA4','#4B79F3','#F8AB8E','#B7B4AA','#D5A956','#A5A582','#C6B870','#F88020','#E79B67','#F7931E','#FFA45C','#ECB65F','#00000029','#454EAB','#B27FA0','#674EB4','#6809EA','#2146C7','#DB528A','#38145E']
+
+console.log("Generating...");
+
+const avatarNameList = [...Array(26)].flatMap((_, i) =>
+    [...Array(26)].map((_, j) => `${String.fromCharCode(65 + i)}${String.fromCharCode(65 + j)}`)
+);
+
+fs.mkdir('avatars', { recursive: true }, (err) => {
+    if (err) {
+        console.log('Failed to create folder.');
+        throw err;
+    }
+});
+
+const canvas = createCanvas(width, height);
+const context = canvas.getContext("2d");
+
+avatarNameList.forEach(avatarName => {
+    // instantiate the canvas object    
+    context.fillStyle = bgColors[Math.floor(Math.random() * bgColors.length)];
+    context.fillRect(0, 0, canvas.width, canvas.height); // draw a background color
+
+    context.font = fontStyle;
+    context.textAlign = 'center'; // set the text to be centered horizontally
+    context.textBaseline = 'middle'; // set the text to be centered vertically
+    context.fillStyle = fontColor;
+    context.fillText(avatarName, canvas.width / 2, canvas.height / 2); // draw the text
+    
+    // Write the image to file
+    const buffer = canvas.toBuffer("image/png");
+
+    const fileName = `avatars/${avatarName}.png`;
+    fs.writeFileSync(fileName, buffer)
+});
+
+console.log(`Generating avatar profile have been end with '${avatarNameList.length}' avatars`);
---
+++ readme.md
@@ -0,0 +1,12 @@
+## Install node library
+```
+npm install
+```
+
+## Run project
+```
+npm run generate
+```
+### Note: 
+- In your machine host, must be installed font as used in source code
+- After run success, all image are store in avatars directory
