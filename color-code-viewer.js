afdd940b25f181f74c09ce9dba1a00fc3ee49c35
chiva_tep <chiva_tep@phillipbank.com.kh>
2024-04-02 11:17:29 +0700

    add fill text

--- color-code-viewer.js
+++ color-code-viewer.js
@@ -1,9 +1,15 @@
-import {createCanvas} from "canvas";
+import {createCanvas, registerFont} from "canvas";
 import fs from "fs";
+
+registerFont("fonts/Euclid Circular B SemiBold.ttf", {family: "Euclid SemiBold"})
 
 // dimensions for the image
 const width = 144;
 const height = 144;
+
+// font style
+const fontStyle = '18px "Euclid SemiBold"'
+const fontColor = "#FFFFFF";
 
 const bgColors = ['#6D5176','#8794BF','#E3A193','#D8BF86','#6ECCAF','#737B63','#334945','#375F48','#557153','#009EFF','#CB8F91','#7D5AA4','#4B79F3','#F8AB8E','#9EC391','#D5A956','#A5A582','#C6B870','#F88020','#E79B67','#F7931E','#FFA45C','#ECB65F','#454EAB','#B27FA0','#674EB4','#6809EA','#2146C7','#DB528A','#38145E']
 
@@ -24,15 +30,16 @@ bgColors.forEach(color => {
     context.fillStyle = color;
     context.fillRect(0, 0, canvas.width, canvas.height); // draw a background color
 
+    context.font = fontStyle;
     context.textAlign = 'center'; // set the text to be centered horizontally
     context.textBaseline = 'middle'; // set the text to be centered vertically
-    // context.fillStyle = fontColor;
-    context.fillText('', canvas.width / 2, canvas.height / 2); // draw the text
+    context.fillStyle = fontColor;
+    context.fillText(color, canvas.width / 2, canvas.height / 2); // draw the text
     
     // Write the image to file
     const buffer = canvas.toBuffer("image/png");
 
-    const fileName = `colors/${color.replace('#', '')}.png`;
+    const fileName = `colors/${color}.png`;
     fs.writeFile(fileName, buffer, (err) => {
         if(err) 
             console.log(`${fileName} was failed.`)
